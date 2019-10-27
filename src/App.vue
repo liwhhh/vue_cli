94af0533c39ea63f26007eae696642be40b103ce
lwhong <li@whong.com>
2019-10-27 18:52:45 +0800

    清空脚手架布局,在App.vue里引入公共样式,字体图标

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
-
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: "App"
+  };
 </script>
 
 <style>
+  @import url("http://at.alicdn.com/t/font_1426139_h6vn3jbl5q.css");
 
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  a {
+    color: inherit;
+  }
+  a:hover {
+    text-decoration: none;
+  }
+  ul,
+  li,
+  ol {
+    list-style: none;
+  }
 </style>
