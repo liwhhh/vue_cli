f70e741eabcfba23ec66ff1523a17da159181f5e
lwhong <li@whong.com>
2019-10-27 19:57:52 +0800

    login页调用子组件边框,子传父,有输入值发射过来父组件,父组件接收,保留到data数据里

--- src/components/sonInput.vue
+++ src/components/sonInput.vue
@@ -1,26 +1,56 @@
 <template>
   <div class="wraperr">
-    <input class="input" :type="type" :placeholder="placeholder"
-    >
+    <input :class="{error:!isOk}"
+     class="input" :type="type" :placeholder="placeholder"
+     @input="dataChange" 
+     @blur="showTips"/>
   </div>
 </template>
 
 <script>
-export default {
-   props:['type','placeholder','err_message','rule']
-}
+  export default {
+    props: ["type", "placeholder", "err_message", "rule"],
+    data() {
+      return {
+        isOk: true
+      };
+    },
+    motheds: {
+      showTips(){//鼠标失去焦点
+      if(!this.isOk){  //不是正则表达式语法就提示用户 那个框格式有误
+        this.$toast.fail(this.err_message);
+      }
+
+      },
+      dataChange($event) { //框值给父组件
+        if (!$event.target.value) {
+          //没有输入值时 边框颜色正常
+          this.isOk = true;
+        } else {
+          //触发了后
+          var regex = new RegExp(this.rule); //正则表达式
+          this.isOk = regex.test($event.target.value); // 3把结果存起来
+        }
+        // 每次数据有所变化,都应该使用事件往外传输,告诉父组件,数据有了变化
+        this.$emit("input", $event.target.value);
+      }
+    }
+  };
 </script>
 
 <style lang="less" scoped>
-.wraperr{
-  margin-top: 5.556vw;
-}
-   .input{
-     width: 100%;
-     border:none;
-     outline:none;//去除边框光线
-     padding: 2.778vw 0;
-     font-size: 5.556vw;
-     border-bottom:1px solid #ccc;
-   }
+  .wraperr {
+    margin-top: 5.556vw;
+  }
+  .input {
+    width: 100%;
+    border: none;
+    outline: none; //去除边框光线
+    padding: 2.778vw 0;
+    font-size: 5.556vw;
+    border-bottom: 1px solid #ccc;
+  }
+  .error {
+    color: red;
+  }
 </style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,43 +1,67 @@
 <template>
   <div class="contanier">
     <span class="iconfont iconjiantou1"></span>
-    <div id="new" >
+    <div id="new">
       <span class="iconfont iconnew"></span>
     </div>
-    <sonInput type="text" placeholder="手机号" 
-    rule="^\d{4,16}$"
-    err-message="请输入正确手机号"
+    <sonInput
+      type="text"
+      placeholder="手机号"
+      rule="^\d{4,16}$"
+      err-message="请输入正确手机号"
+      @input="setUserName"
+    ></sonInput>
+    <sonInput
+      type="password"
+      placeholder="密码"
+      rule="^\d{3,16}$"
+      err-message="请输入正确密码"
+      @input="setUserPwd"
     ></sonInput>
   </div>
 </template>
 
 <script>
-import sonInput from '../components/sonInput.vue';
-export default {
-   components:{
-     sonInput:sonInput
-   }
-}
+  import sonInput from "../components/sonInput.vue";
+  export default {
+    components: {
+      sonInput: sonInput
+    },
+    data(){
+      return{
+        username:"",
+        password:""
+      }
+    },
+    methods:{
+      //1.调用子组件的边框,输入框的值发射过来父组件,保存到data里
+      setUserName(name){
+        this.username=name;
+      },
+      setUserPwd(name){
+        this.password=name;
+      }
+    }
+  };
 </script>
 
 <style lang="less" scoped>
- .contanier{
-   padding:5.556vw;
-   
- }
- .iconjiantou1{
-   font-size: 7.5vw;
- }
- #new{
-   width: 100%;
-   height: 100%;
-   display: flex;
-   justify-content: center;//主轴居中
- }
- .iconnew{
-   width: 35vw;
-   font-size: 35vw;
-   color: #d81e06;
-   height: 35vw;
- }
+  .contanier {
+    padding: 5.556vw;
+  }
+  .iconjiantou1 {
+    font-size: 7.5vw;
+  }
+  #new {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center; //主轴居中
+  }
+  .iconnew {
+    width: 35vw;
+    font-size: 35vw;
+    color: #d81e06;
+    height: 35vw;
+  }
 </style>
