ee64e848388708ab557669c9f127f490c5563fb2
lwhong <li@whong.com>
2019-10-27 19:16:05 +0800

    login的头部样式在当前页完成

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>登录页</div>
+  <div class="contanier">
+    <span class="iconfont iconjiantou1"></span>
+    <div id="new" >
+      <span class="iconfont iconnew"></span>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -8,6 +14,24 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+ .contanier{
+   padding:5.556vw;
+   
+ }
+ .iconjiantou1{
+   font-size: 7.5vw;
+ }
+ #new{
+   width: 100%;
+   height: 100%;
+   display: flex;
+   justify-content: center;//主轴居中
+ }
+ .iconnew{
+   width: 35vw;
+   font-size: 35vw;
+   color: #d81e06;
+   height: 35vw;
+ }
 </style>
